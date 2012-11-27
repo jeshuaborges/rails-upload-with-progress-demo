b1eabba160ee4aad791645a74372ab4f723206be
Jeshua Borges <jesh@jesh.me>
2012-11-27 11:42:33 -0800

    Init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUploadWithProgressDemo::Application.config.secret_token = '124c557065f1e8f2eb0b944d67896503d5f1d154bf80e0d8745928cc8bd7161de3d51e1df27ddb9626fda16e7374f9823499b3e86c00d010ae7550d45663fcc7'
